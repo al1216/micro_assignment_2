42e4ad2cb9f4f0bb14681b322f4fcd62c75a26bf
Aditya Kumar Gupta <adityakumargupta115@gmail.com>
2023-03-29 16:05:48 +0530

    added all the skill sets in the array for rendering purpose, almost completed the remaining design parts

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,30 +1,73 @@
-import React from 'react'
-import './Form.css';
+import React, { useState } from "react";
+import "./Form.css";
 
 function Form() {
+  let [name, setName] = useState("");
+  let [email, setEmail] = useState("");
+  let [password, setPassword] = useState("");
+  let [myArr, setArr] = useState([]);
   return (
     <>
-    <div className="form">
+      <div className="form">
         <div className="input-fields">
-            <input type="text" className="name" placeholder='Your Name'/>
-            <input type="email" className="email" placeholder='Email Address'/>
-            <input type="password" className="password" placeholder='Password'/>
-            <select name="skills" className="skills">
-            <option value="" disabled selected>Choose your skills</option>
-                <option value="HTML">HTML</option>
-                <option value="CSS">CSS</option>
-                <option value="JS">JS</option>
-                <option value="REACT">REACT</option>
-            </select>
-            <div className="skills-selected">
-                {/* the skills will come here */}
+          <input
+            type="text"
+            className="name"
+            placeholder="Your Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            className="email"
+            placeholder="Email Address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            className="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <select
+            name="skills"
+            className="skills"
+            onChange={(e) => {
+              let temp = e.target.value;
+              if (!myArr.includes(temp)) setArr((arr) => [...arr, temp]);
+            }}
+          >
+            <option value="" selected disabled>
+              Choose your skills
+            </option>
+            <option value="HTML">HTML</option>
+            <option value="CSS">CSS</option>
+            <option value="JS">JS</option>
+            <option value="REACT">REACT</option>
+          </select>
+          <div className="skills-selected">
+            <div className="skill">
+              <p className="skill-para">HTML</p>
+              <p className="cross">X</p>
             </div>
-            <button disabled="disabled" className="btn">CLAIM YOUR FREE TRIAL</button>
+            <div className="skill">
+              <p className="skill-para">HTML</p>
+              <p className="cross">X</p>
+            </div>
+          </div>
+          <button disabled="disabled" className="btn">
+            CLAIM YOUR FREE TRIAL
+          </button>
         </div>
-        <p className="tAndc">By clicking the button you are agreeing to our <span className="para-tc"> Terms and Services</span></p>
-    </div>
+        <p className="tAndc">
+          By clicking the button you are agreeing to our{" "}
+          <span className="para-tc"> Terms and Services</span>
+        </p>
+      </div>
     </>
-  )
+  );
 }
 
 export default Form;
